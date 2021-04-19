976bd2830c9f2c3932ec9164fc16b7b1c92a39f2
mayanwei1 <mayanwei1@gome.com.cn>
2021-04-19 10:13:00 +0800

    update _4_寻找两个正序数组的中位数

--- src/test/java/leetcode/arrays/_4_寻找两个正序数组的中位数Test.java
+++ src/test/java/leetcode/arrays/_4_寻找两个正序数组的中位数Test.java
@@ -16,4 +16,12 @@ class _4_寻找两个正序数组的中位数Test {
         Assert.assertEquals(4.0,media,2);
 
     }
+
+    @Test
+    public void testSolution2(){
+        _4_寻找两个正序数组的中位数.Solution2 solution2 = new  _4_寻找两个正序数组的中位数.Solution2();
+        double media = solution2.findMedianSortedArrays( new int[]{ 1, 3, 4, 9 }, new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9 } );
+        Assert.assertEquals(4.0,media,2);
+
+    }
 }
