0f9d3fbd10b1a3f46ce92742458ca41ffffc35d1
mayanwei1 <mayanwei1@gome.com.cn>
2021-06-16 13:42:01 +0800

    update JZ64滑动窗口的最大值.java

--- src/main/java/leetcode/jzhoffer/JZ64滑动窗口的最大值.java
+++ src/main/java/leetcode/jzhoffer/JZ64滑动窗口的最大值.java
@@ -2,6 +2,8 @@ package leetcode.jzhoffer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值。例如，如果输入数组{2,3,4,2,6,2,5,1}及滑动窗口的大小3，那么一共存在6个滑动窗口，
@@ -28,6 +30,32 @@ public class JZ64滑动窗口的最大值 {
             return res;
         }
 
+    }
+
+    static class Solution2{
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            if(nums.length == 0 || k == 0) return new int[0];
+            Deque<Integer> deque = new LinkedList<>();
+            int[] res = new int[nums.length - k + 1];
+            // 未形成窗口
+            for(int i = 0; i < k; i++) {
+                while(!deque.isEmpty() && deque.peekLast() < nums[i])
+                    deque.removeLast();
+                deque.addLast(nums[i]);
+            }
+            res[0] = deque.peekFirst();
+            // 形成窗口后
+            for(int i = k; i < nums.length; i++) {
+                if(deque.peekFirst() == nums[i - k])
+                    deque.removeFirst();
+                while(!deque.isEmpty() && deque.peekLast() < nums[i])
+                    deque.removeLast();
+                deque.addLast(nums[i]);
+                res[i - k + 1] = deque.peekFirst();
+            }
+            return res;
+
+        }
     }
 
     public static void main(String[] args) {
