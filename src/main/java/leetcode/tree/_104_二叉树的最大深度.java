85be56d862aaeb672f22ed955651e6485fcb1217
mayanwei1 <mayanwei1@gome.com.cn>
2022-06-12 21:09:55 +0800

    树 102 二叉树的最大深度

--- src/main/java/leetcode/tree/_104_二叉树的最大深度.java
+++ src/main/java/leetcode/tree/_104_二叉树的最大深度.java
@@ -21,7 +21,7 @@ package leetcode.tree;
  * @author mayanwei
  * @date 2022-06-12.
  */
-public class _102_二叉树的最大深度{
+public class _104_二叉树的最大深度{
     class Solution {
         private Integer maxDepth = 0;
         public int maxDepth(TreeNode root) {
