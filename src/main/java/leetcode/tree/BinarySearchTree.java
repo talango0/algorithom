103f92a5761f5c668d8864cf774a7735563c137b
mayanwei1 <mayanwei1@gome.com.cn>
2022-06-12 21:00:45 +0800

    BST 增删改模式

--- src/main/java/leetcode/tree/BinarySearchTree.java
+++ src/main/java/leetcode/tree/BinarySearchTree.java
@@ -5,6 +5,21 @@ import common.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+/**
+ * 1、如果当前节点会对下面的子节点有整体影响，可以通过辅助函数增长参数列表，借助参数传递信息。
+ *
+ * 2、在二叉树递归框架之上，扩展出一套 BST 代码框架：
+ *
+ * void BST(TreeNode root, int target) {
+ *     if (root.val == target)
+ *         // 找到目标，做点什么
+ *     if (root.val < target)
+ *         BST(root.right, target);
+ *     if (root.val > target)
+ *         BST(root.left, target);
+ * }
+ * 3、根据代码框架掌握了 BST 的增删查改操作。
+ */
 
 /**
  * @author mayanwei
