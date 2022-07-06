2add3a6bb45dc72b0e655f0f51a0ac71f3dc9460
mayanwei1 <Gome@2597>
2022-07-06 22:55:48 +0800

    二叉树算法思路篇

---
+++ src/main/java/leetcode/tree/_114_二叉树展开为链表.java
@@ -0,0 +1,81 @@
+package leetcode.tree;
+//给你二叉树的根结点 root ，请你将它展开为一个单链表：
+//
+//
+// 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+// 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,2,5,3,4,null,6]
+//输出：[1,null,2,null,3,null,4,null,5,null,6]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [0]
+//输出：[0]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中结点数在范围 [0, 2000] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶：你可以使用原地算法（O(1) 额外空间）展开这棵树吗？
+// Related Topics 栈 树 深度优先搜索 链表 二叉树 👍 1226 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class _114_二叉树展开为链表 {
+
+    class Solution {
+        //定义，输入节点root，然后 root 为根的二叉树就会被拉成一条链条
+        //对于一个节点 x， 可以执行如下流程
+        // 1. 先利用flatten(x.left) 和 flatten(x.right) 将左右子树拉平
+        // 2. 然后将x的右子树连接到左子树下方，然后将整个左子树作为x的右子树。
+        // 这样，以 x 为根的整颗二叉树就拉平了，恰好完成了 flatten(X) 的定义。
+        public void flatten(TreeNode root) {
+            //base case
+            if (root == null) {return;}
+            //利用定义，把左右子树拉平
+            flatten(root.left);
+            flatten(root.right);
+
+            /**后序遍历的位置 */
+            //1. 左右子树已经被拉成了一条链表
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+
+            //2. 将左子树作为右子树
+            root.left = null;
+            root.right = left;
+
+            //3. 将原先的右子树连接到当前有节点的末端
+            TreeNode p = root;
+            while (p.right != null) {
+                p = p.right;
+            }
+            p.right = right;
+
+        }
+    }
+}
---
+++ src/main/java/leetcode/tree/_226_反转二叉树.java
@@ -0,0 +1,93 @@
+package leetcode.tree;
+//给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+//
+//
+// 示例 2：
+//
+//
+//
+//
+//输入：root = [2,1,3]
+//输出：[2,3,1]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目范围在 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1339 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+public class _226_反转二叉树 {
+
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            //遍历二叉树
+            traverse(root);
+            return root;
+        }
+        private void traverse(TreeNode node) {
+            if(node == null){
+                return;
+            }
+            //前序位置
+            //每一个节点需要做的事情就是交换它的左右子节点
+            TreeNode left = node.left;
+            TreeNode right = node.right;
+            node.right = left;
+            node.left = right;
+            //遍历框架去递归遍历左右子树
+            traverse(left);
+            traverse(right);
+
+        }
+    }
+
+    class Solution2 {
+        /**
+         * 用分解思维
+         * 定义：将以root 为根的这颗二叉树翻转，返回翻转后的二叉树的根节点
+         * @param root
+         * @return
+         */
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return root;
+            }
+            //利用函数定义先翻转左右子树
+            TreeNode left = invertTree(root.left);
+            TreeNode right = invertTree(root.right);
+
+            //然后交换左右子节点
+            root.left = right;
+            root.right = left;
+
+            //和定义逻辑一样，以root为根的这棵树已经被翻转，返回root
+            return root;
+        }
+
+    }
+}
