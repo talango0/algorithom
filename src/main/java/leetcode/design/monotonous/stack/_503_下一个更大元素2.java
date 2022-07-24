cc92121fb89fea500e1005da5dd63d99127c7e8b
mayanwei1 <mayanwei1@gome.com.cn>
2022-07-24 12:28:29 +0800

    单调栈、单调队列

--- src/main/java/leetcode/design/monotonous/stack/_503_下一个更大元素2.java
+++ src/main/java/leetcode/design/monotonous/stack/_503_下一个更大元素2.java
@@ -1,4 +1,4 @@
-package leetcode.arrays.monotonous.stack;
+package leetcode.design.monotonous.stack;
 //给定一个循环数组 nums （ nums[nums.length - 1] 的下一个元素是 nums[0] ），返回 nums 中每个元素的 下一个更大元素
 // 。
 //
--- src/main/java/leetcode/design/monotonous/stack/_739_每日温度.java
+++ src/main/java/leetcode/design/monotonous/stack/_739_每日温度.java
@@ -1,4 +1,4 @@
-package leetcode.arrays.monotonous.stack;
+package leetcode.design.monotonous.stack;
 //请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
 //
 //
