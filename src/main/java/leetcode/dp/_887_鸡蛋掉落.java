8e14806d4dc7e13694b1e5f497a9f7e9e1f67d6e
mayanwei1 <mayanwei1@gome.com.cn>
2022-07-30 21:51:12 +0800

    动态规划：扔鸡蛋

--- src/main/java/leetcode/dp/_887_鸡蛋掉落.java
+++ src/main/java/leetcode/dp/_887_鸡蛋掉落.java
@@ -153,4 +153,25 @@ public class _887_鸡蛋掉落{
         }
 
     }
+
+    class Solution3 {
+
+        public int superEggDrop(int K, int N) {
+            int remainTestCount = 1;//穷举移动次数（测试的次数）
+            while (getConfirmFloors(remainTestCount, K) < N){
+                ++remainTestCount;
+            }
+            return remainTestCount;
+        }
+
+        //在remainTestCount个测试机会（扔鸡蛋的机会 或者移动的次数），eggsCount个鸡蛋可以确定的楼层数量
+        public int getConfirmFloors(int remainTestCount, int eggsCount){
+            if (remainTestCount == 1 || eggsCount == 1){
+                //如果remainTestCount == 1你只能移动一次，则你只能确定第一楼是否，也就是说鸡蛋只能放在第一楼，如果碎了，则F == 0，如果鸡蛋没碎，则F == 1
+                //如果eggsCount == 1鸡蛋数为1，它碎了你就没有鸡蛋了，为了保险，你只能从第一楼开始逐渐往上测试，如果第一楼碎了（同上），第一楼没碎继续测第i楼，蛋式你不可能无限制的测试，因为你只能测试remainTestCount次
+                return remainTestCount;
+            }
+            return getConfirmFloors(remainTestCount - 1, eggsCount - 1) + 1 + getConfirmFloors(remainTestCount - 1, eggsCount);
+        }
+    }
 }
