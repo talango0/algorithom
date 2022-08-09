18919b1a151387416c0e7febb97a678668d58fd0
mayanwei1 <mayanwei1@gome.com.cn>
2022-08-09 15:09:45 +0800

    双指针之 接雨水

---
+++ src/main/java/leetcode/arrays/_11_盛最多水的容器.java
@@ -0,0 +1,66 @@
+package leetcode.arrays;
+//给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+//
+// 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+//
+// 返回容器可以储存的最大水量。
+//
+// 说明：你不能倾斜容器。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：[1,8,6,2,5,4,8,3,7]
+//输出：49
+//解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+//
+// 示例 2：
+//
+//
+//输入：height = [1,1]
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// n == height.length
+// 2 <= n <= 10⁵
+// 0 <= height[i] <= 10⁴
+//
+//
+// Related Topics 贪心 数组 双指针 👍 3711 👎 0
+
+/**
+ * @author mayanwei
+ * @date 2022-08-09.
+ */
+public class _11_盛最多水的容器{
+    class Solution{
+        // 本题中竖线没有宽度，所以 left 和 rightright 之间能够盛的水就是 min(height[left], height[right]) * (right-left)
+        // 用 left 和 right 两个指针从两端向中心收缩，一边收缩一边计算 [left, right] 之间的矩形面积，取最大的面积值即是答案
+        public int maxArea(int[] height) {
+            int left = 0, right = height.length - 1;
+            int res = 0;
+            while (left < right) {
+                // [left, right] 之间的矩形面积
+                int cur_area = Math.min(height[left], height[right]) * (right - left);
+                res = Math.max(res, cur_area);
+                // 双指针技巧，移动较低的一边
+                if (height[left] < height[right]) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ src/main/java/leetcode/arrays/_42_接雨水.java
@@ -0,0 +1,113 @@
+package leetcode.arrays;
+//给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+//输出：6
+//解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+//
+//
+// 示例 2：
+//
+//
+//输入：height = [4,2,0,3,2,5]
+//输出：9
+//
+//
+//
+//
+// 提示：
+//
+//
+// n == height.length
+// 1 <= n <= 2 * 10⁴
+// 0 <= height[i] <= 10⁵
+//
+//
+// Related Topics 栈 数组 双指针 动态规划 单调栈 👍 3676 👎 0
+
+/**
+ * @see _11_盛最多水的容器
+ * @author mayanwei
+ * @date 2022-08-09.
+ */
+public class _42_接雨水{
+    //暴力法
+
+    class Solution{
+        public int trap(int[] height) {
+            int n = height.length;
+            int res = 0;
+            for (int i = 1; i < n - 1; i++) {
+                int l_max = 0, r_max = 0;
+                // 找右边最高的柱子
+                for (int j = i; j < n; j++) {
+                    r_max = Math.max(r_max, height[j]);
+                }
+                // 找左边最高的柱子
+                for (int j = i; j >= 0; j--) {
+                    l_max = Math.max(l_max, height[j]);
+                }
+                // 如果自己就是最高的话
+                // l_max = r_max = height[i]
+                res += Math.min(l_max, r_max) - height[i];
+            }
+            return res;
+        }
+    }
+
+    //备忘录优化
+
+    class Solution2{
+        public int trap(int[] height) {
+            int n = height.length;
+            int res = 0;
+            // 数组充当备忘录
+            int[] l_max = new int[n];
+            int[] r_max = new int[n];
+            // 初始化 base case
+            l_max[0] = height[0];
+            r_max[n - 1] = height[n - 1];
+            for (int i = 1; i < n; i++) {
+                l_max[i] = Math.max(height[i], l_max[i - 1]);
+            }
+            for (int i = n - 2; i >= 0; i--) {
+                r_max[i] = Math.max(height[i], r_max[i + 1]);
+            }
+            for (int i = 1; i < n - 1; i++) {
+                res += Math.min(l_max[i], r_max[i]) - height[i];
+            }
+            return res;
+        }
+    }
+
+    //双指针解法
+
+    class Solution3{
+        public int trap(int[] height) {
+            int left = 0, right = height.length - 1;
+            int res = 0;
+            int l_max = 0, r_max = 0;
+            while (left < right) {
+                l_max = Math.max(l_max, height[left]);
+                r_max = Math.max(r_max, height[right]);
+                // res += min(l_max, r_max)-height[i]
+                if (l_max < r_max) {
+                    res += l_max - height[left];
+                    left++;
+                }
+                else {
+                    res += r_max - height[right];
+                    right--;
+                }
+            }
+            return res;
+        }
+    }
+}
