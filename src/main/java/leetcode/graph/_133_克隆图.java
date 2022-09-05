e360802596cceb6a07839946a406e6aac50a88d2
mayanwei1 <Gome@2597>
2022-09-05 18:32:26 +0800

    哈希表、深度优先遍历 克隆图

--- src/main/java/leetcode/graph/_133_克隆图.java
+++ src/main/java/leetcode/graph/_133_克隆图.java
@@ -103,4 +103,26 @@ public class _133_克隆图 {
         }
     }
 
+
+    class Solution2 {
+        private HashMap<Node, Node> visited = new HashMap<>();
+
+        public Node cloneGraph(Node node) {
+            if (node == null) {
+                return null;
+            }
+
+            if (visited.containsKey(node)) {
+                return visited.get(node);
+            }
+
+            Node cloneNode = new Node(node.val);
+            visited.put(node, cloneNode);
+            for (Node neighbor : node.neighbors) {
+                cloneNode.neighbors.add(cloneGraph(neighbor));
+            }
+            return cloneNode;
+        }
+    }
+
 }
