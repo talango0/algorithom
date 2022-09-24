29068cf21ec3e8b9bfea1af9d01792621b22ca1b
mayanwei1 <mayanwei1@gome.com.cn>
2022-09-24 20:33:47 +0800

    颠倒二进制位

--- src/main/java/leetcode/math/bitop/BitOperation.java
+++ src/main/java/leetcode/math/bitop/BitOperation.java
@@ -128,4 +128,54 @@ public class BitOperation{
         Assert.assertArrayEquals(new int[]{1,1,1,1,1}, a);
 
     }
+
+
+
+    @Test
+    public void test7(){
+        int i = -1;
+        System.out.println(String.format("%32s",Integer.toBinaryString(i)).replaceAll(" ", "0"));
+
+        i >>>= -1;
+
+        System.out.println(String.format("%32s",Integer.toBinaryString(i)).replaceAll(" ", "0"));
+        System.out.println();
+    }
+
+    static class Int2Binary {
+        private static String binaryString;
+
+        private static void int2binary(int n) {
+            binaryString = Integer.toBinaryString(n);
+        }
+
+        private static void addPadding(){
+            binaryString = String.format("%32s", binaryString).replaceAll(" ", "0");
+        }
+
+        private static void addBlocks(String separator) {
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i< binaryString.length(); i++ ){
+                sb.append(binaryString.charAt(i));
+                if (i%4 == 3 && i != binaryString.length() - 1) {
+                    sb.append(separator);
+                }
+            }
+            binaryString = sb.toString();
+        }
+
+        public static void convert2Binary(int n) {
+            int2binary(n);
+            addPadding();
+            addBlocks("_");
+        }
+
+        public static void main(String[] args) {
+            int n = 10;
+            for (int i = -5; i <= 5; i++){
+                convert2Binary(i);
+                System.out.format("Binary representation of %2d is %s\n", i, binaryString);
+            }
+        }
+    }
 }
