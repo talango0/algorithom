863fd64d5cf88d97bcbc216f8a35b90c88dcb1f4
mayanwei1 <mayanwei1@gome.com.cn>
2022-09-24 12:48:03 +0800

    双指针 哈希检测环 快乐数

---
+++ src/main/java/leetcode/math/_202_快乐数.java
@@ -0,0 +1,105 @@
+package leetcode.math;
+//编写一个算法来判断一个数 n 是不是快乐数。
+//
+// 「快乐数」 定义为：
+//
+//
+// 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+// 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+// 如果这个过程 结果为 1，那么这个数就是快乐数。
+//
+//
+// 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：n = 19
+//输出：true
+//解释：
+//1² + 9² = 82
+//8² + 2² = 68
+//6² + 8² = 100
+//1² + 0² + 0² = 1
+//
+//
+// 示例 2：
+//
+//
+//输入：n = 2
+//输出：false
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= n <= 2³¹ - 1
+//
+//
+// Related Topics 哈希表 数学 双指针 👍 1073 👎 0
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author mayanwei
+ * @date 2022-09-24.
+ */
+public class _202_快乐数{
+    /**
+     * 用哈希集合检测循环
+     * 时间复杂度 O(logn)
+     * 空间负载度 O(logn)
+     */
+    class Solution{
+        private int getNext(int n) {
+            int totalSum = 0;
+            while (n > 0) {
+                int d = n % 10;
+                n = n / 10;
+                totalSum += d * d;
+            }
+            return totalSum;
+        }
+
+        public boolean isHappy(int n) {
+            Set<Integer> seen = new HashSet<>();
+            while (n != 1 && !seen.contains(n)) {
+                seen.add(n);
+                n = getNext(n);
+            }
+            return n == 1;
+        }
+    }
+
+    /**
+     * 用快慢指针
+     * 时间复杂度 O(logn)
+     * 空间负载度 O(1)
+     */
+    class Solution2{
+        public int getNext(int n) {
+            int totalSum = 0;
+            while (n > 0) {
+                int d = n % 10;
+                n = n / 10;
+                totalSum += d * d;
+            }
+            return totalSum;
+        }
+
+        public boolean isHappy(int n) {
+            int slowRunner = n;
+            int fastRunner = getNext(n);
+            while (fastRunner != 1 && slowRunner != fastRunner) {
+                slowRunner = getNext(slowRunner);
+                fastRunner = getNext(getNext(fastRunner));
+            }
+            return fastRunner == 1;
+        }
+    }
+}
