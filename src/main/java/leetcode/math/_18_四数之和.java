c59c5fa7a09c46a420a611165c42f6d0f9274ad2
mayanwei1 <mayanwei1@gome.com.cn>
2022-09-25 16:02:03 +0800

    分组+哈希表 四数相加

---
+++ src/main/java/leetcode/math/_18_四数之和.java
@@ -0,0 +1,117 @@
+package leetcode.math;
+//给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[
+//b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
+//
+//
+// 0 <= a, b, c, d < n
+// a、b、c 和 d 互不相同
+// nums[a] + nums[b] + nums[c] + nums[d] == target
+//
+//
+// 你可以按 任意顺序 返回答案 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,0,-1,0,-2,2], target = 0
+//输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [2,2,2,2,2], target = 8
+//输出：[[2,2,2,2]]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 200
+// -10⁹ <= nums[i] <= 10⁹
+// -10⁹ <= target <= 10⁹
+//
+//
+// Related Topics 数组 双指针 排序 👍 1386 👎 0
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author mayanwei
+ * @date 2022-09-25.
+ */
+public class _18_四数之和{
+    /**
+     * 最朴素的方法是使用四重循环枚举所有的四元组，然后使用哈希表进行去重操作，得到不包含重复四元组的最终答案。假设数组的长度是
+     * n
+     * n，则该方法中，枚举的时间复杂度为 O(n^4)，去重操作的时间复杂度和空间复杂度也很高，因此需要换一种思路。
+     * <p>
+     * 为了避免枚举到重复四元组，则需要保证每一重循环枚举到的元素不小于其上一重循环枚举到的元素，且在同一重循环中不能多次枚举到相同的元素。
+     * <p>
+     * 为了实现上述要求，可以对数组进行排序，并且在循环过程中遵循以下两点：
+     * <p>
+     * 1.每一种循环枚举到的下标必须大于上一重循环枚举到的下标；
+     * 2.同一重循环中，如果当前元素与上一个元素相同，则跳过当前元素。
+     */
+    class Solution{
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            List<List<Integer>> quadruplets = new ArrayList<List<Integer>>();
+            if (nums == null || nums.length < 4) {
+                return quadruplets;
+            }
+            Arrays.sort(nums);
+            int length = nums.length;
+            for (int i = 0; i < length - 3; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                    break;
+                }
+                if ((long) nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+                    continue;
+                }
+                for (int j = i + 1; j < length - 2; j++) {
+                    if (j > i + 1 && nums[j] == nums[j - 1]) {
+                        continue;
+                    }
+                    if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                        break;
+                    }
+                    if ((long) nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+                        continue;
+                    }
+                    int left = j + 1, right = length - 1;
+                    while (left < right) {
+                        long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                        if (sum == target) {
+                            quadruplets.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                            while (left < right && nums[left] == nums[left + 1]) {
+                                left++;
+                            }
+                            left++;
+                            while (left < right && nums[right] == nums[right - 1]) {
+                                right--;
+                            }
+                            right--;
+                        }
+                        else if (sum < target) {
+                            left++;
+                        }
+                        else {
+                            right--;
+                        }
+                    }
+                }
+            }
+            return quadruplets;
+        }
+    }
+}
