575675d281f1635f83cd50e660e9d7bf7575929d
mayanwei1 <mayanwei1@gome.com.cn>
2022-09-30 08:29:09 +0800

    暴力 Manacher 最长回文子串

--- src/main/java/leetcode/string/_647_回文子串.java
+++ src/main/java/leetcode/string/_647_回文子串.java
@@ -63,4 +63,42 @@ public class _647_回文子串{
             return ans;
         }
     }
+
+
+    class Solution2{
+        /**
+         * Manacher
+         * 算法是时间复杂度 O(n),空间复杂度 O(n)
+         */
+        public int countSubstrings(String s) {
+            int n = s.length();
+            StringBuffer t = new StringBuffer("$#");
+            for (int i = 0; i < n; i++) {
+                t.append(s.charAt(i));
+                t.append('#');
+            }
+            n = t.length();
+            t.append('!');
+
+            int[] f = new int[n];
+            int iMax = 0, rMax = 0, ans = 0;
+            for (int i = 1; i < n; i++) {
+                // 初始化 f[i]
+                f[i] = i < rMax ? Math.min(rMax - i + 1, f[2 * iMax - i]) :1;
+                // 中心拓展
+                while (t.charAt(i + f[i]) == t.charAt(i - f[i])) {
+                    f[i]++;
+                }
+                // 动态维护 iMax 和 rMax
+                if (i + f[i] - 1 > rMax) {
+                    iMax = i;
+                    rMax = i + f[i] - 1;
+                }
+                // 统计答案，当前贡献 (f[i]-1)/2 上取整
+                ans += f[i] / 2;
+            }
+
+            return ans;
+        }
+    }
 }
