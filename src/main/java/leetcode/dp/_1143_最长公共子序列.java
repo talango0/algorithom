4542ed2a68331230afba28339ae26ab96139c5e0
mayanwei1 <mayanwei1@gome.com.cn>
2022-10-19 23:39:44 +0800

    最长公共子序列

--- src/main/java/leetcode/dp/_1143_最长公共子序列.java
+++ src/main/java/leetcode/dp/_1143_最长公共子序列.java
@@ -107,12 +107,18 @@ public class _1143_最长公共子序列{
         public int longestCommonSubsequence(String text1, String text2) {
             int m = text1.length();
             int n = text2.length();
+            // 定义 text1[0..i-1] 和 text2[0..j-1] 的 lcs 长度为 dp[i][j]
+            // 目标 text1[0..i-1] 和 text2[0..j-1] 的 lcs 长度， 即 dp[m][n]
+            // base case: dp[0][..] = dp[..][0] = 0
             int dp[][] = new int[m+1][n+1];
             for(int i = 1; i <= m; i++){
                 for(int j = 1; j <= n; j++){
+                    // 现在 i 和 j 从1 开始，所以要减 1
                     if(text1.charAt(i-1) == text2.charAt(j-1)){
+                        // text1[i-1] 和 text[j-1] 必然在 lcs 中
                         dp[i][j] = dp[i-1][j-1] + 1;
                     }else {
+                        // text1[i-1] 和 text[j-1] 至少有一个不在 lcs 中
                         dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                     }
                 }
