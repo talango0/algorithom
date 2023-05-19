e81cea83eaa91769e822e1bff39c92642b6ca6cc
mayanwei1 <mayanwei1@gome.com.cn>
2023-05-19 14:39:19 +0800

    程序员面试金典_05_08_绘制直线

--- src/main/java/leetcode/程序员面试金典/_05_08_绘制直线.java
+++ src/main/java/leetcode/程序员面试金典/_05_08_绘制直线.java
@@ -31,13 +31,14 @@ import java.util.Arrays;
 //来源：力扣（LeetCode）
 //链接：https://leetcode.cn/problems/draw-line-lcci
 //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
 /**
  * @author mayanwei
  */
 public class _05_08_绘制直线{
 
 
-    static class Solution {
+    static class Solution{
         public int[] drawLine(int length, int w, int x1, int x2, int y) {
             int[] ret = new int[length];
             // 注意根据所在行数计算偏移量
@@ -58,6 +59,54 @@ public class _05_08_绘制直线{
         }
     }
 
+    // 绘制直线。有个单色屏幕存储在一个一维字节数组中，使得8个连续像素可以存放 在一个字节里。
+    // 屏幕宽度为 w，且 w 可被 8 整除(即一个字节不会分布在两行上)，屏幕高度 6 可由数组长度及屏幕宽度推算得出。
+    // 请实现一个函数，绘制从点(x1, y )到点(x2, y )的水平线。
+
+    /**
+     * 这个题目有个简单解法，用for循环迭代，从x1到 x2,一路设定每个像素。但是这么做没劲，效率也不高。
+     * 更高的做法是，如果 x1 和 x2 相距甚远，其间包含几个完整字节，只要使用 screen[bytes_pos] = 0xFF，一次就能设置一整个字节。
+     * 这条线起点和终点剩余部分的位，可用掩码设定。
+     */
+    class Solution1{
+        public void drawLine(byte[] screen, int width, int x1, int x2, int y) {
+            int start_offset = x1 % 8;
+            int first_full_bytes = x1 / 8;
+            if (start_offset != 0) {
+                first_full_bytes++;
+            }
+
+            int end_offset = x2 % 8;
+            int last_full_bytes = x2 / 8;
+            if (last_full_bytes != 0) {
+                last_full_bytes--;
+            }
+
+            //设置完整的字节
+            for (int i = first_full_bytes; i <= last_full_bytes; i++) {
+                screen[(width / 8) * y + i] = (byte) 0xff;
+            }
+            //创建开始行和结束行的掩码
+            byte start_mask = (byte) (0xff >> start_offset);
+            byte end_mask = (byte) (0xff >> end_offset);
+
+            //设置开始与结束行
+            if ((x1 / 8) == (x2 / 8)) { // x1和 x2 在同一字节
+                byte mask = (byte) (start_mask & end_mask);
+                screen[(width / 8) * y + (x1 / 8)] |= mask;
+            }
+            else {
+                if (start_mask != 0) {
+                    int byte_number = (width / 8) * y + first_full_bytes - 1;
+                    screen[byte_number] |= start_mask;
+                }
+                if (end_mask != 0) {
+                    int byte_number = (width) / 8 * y + last_full_bytes + 1;
+                    screen[byte_number] |= end_mask;
+                }
+            }
+        }
+    }
 
 
     public static void main(String[] args) {
